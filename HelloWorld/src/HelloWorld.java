1907aa2e72add4ef89426454f6c5cbe79a14e266
MarcVauderwange <MarcVauderwange@0-208-04>
2020-09-25 12:00:06 +0200

    FizzBuzz

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,18 +1,27 @@
 
 public class HelloWorld {
 
-	static int counter = 0;
+	static int counter = 1;
+	
+	// Dies ist ein Zählspiel
 	
 	public static void main(String[] args) {
 		
 		while(counter<100) {
-		if(counter%5==0) {System.out.println("Hello Lukkkkkkkkkas");}
-		/* else {System.out.println("Hello World!");}
-		System.out.println(counter); */
+			if(counter%3==0 && counter%5==0) {
+				System.out.println("FizzBuzz");}
+				else {
+			
+					if(counter%3==0) {System.out.println("Fizz");}
+						else {
+								if(counter%5==0)	{System.out.println("Buzz");}
+				
+						else {System.out.println(counter);}
+						}
+			}
 		counter++;
-		
-		
 		}
 	}
+
 
 }
